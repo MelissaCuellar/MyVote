6bf15bfc1e36fa6bd972cc24c67dbc2e1259b3f7
MelissaCuellar <43379979+MelissaCuellar@users.noreply.github.com>
2019-04-20 11:39:33 -0500

    API VotingEventsController

---
+++ MyVote.Web/Controllers/API/VotingEventsController.cs
@@ -0,0 +1,23 @@
+﻿
+namespace MyVote.Web.Controllers.API
+{
+    using Microsoft.AspNetCore.Mvc;
+    using MyVote.Web.Data.Repositories;
+
+    [Route("api/[Controller]")]
+    public class VotingEventsController:Controller
+    {
+        private readonly IVotingEventRepository votingEventRepository;
+
+        public VotingEventsController(IVotingEventRepository votingEventRepository)
+        {
+            this.votingEventRepository = votingEventRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetVotingEvents()
+        {
+            return Ok(this.votingEventRepository.GetAll());
+        }
+    }
+}
